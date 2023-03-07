3fa514627e78a59dd6d4d0632869a3073bc35b7e
Yannick Müller <yannick.mueller@fusiva.ch>
2023-03-07 17:37:59 +0100

    fixed wrong url paths

--- controllers/api_v1_Appointment.h
+++ controllers/api_v1_Appointment.h
@@ -16,8 +16,8 @@ namespace api
     {
     public:
       METHOD_LIST_BEGIN
-      METHOD_ADD(Appointment::getAppointmentByClassId, "/{1:the_class_id}", Get);
-      METHOD_ADD(Appointment::getFutureByClassId, "/active/{1:the_class_id}");
+      METHOD_ADD(Appointment::getAppointmentByClassId, "?classId={1:the_class_id}", Get);
+      METHOD_ADD(Appointment::getFutureByClassId, "/active/?classId={1:the_class_id}");
       METHOD_LIST_END
       void getAppointmentByClassId(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, const int classId);
       void getFutureByClassId(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, const int classId);
